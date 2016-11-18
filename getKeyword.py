809f0c9e69b1bce0e6b481cca9579db1e1a28f1f
歐博文 <wen94319@gmail.com>
2016-11-18 12:48:54 +0800

    add code

---
+++ getKeyword.py
@@ -0,0 +1,16 @@
+import requests
+import json
+r = requests.get("http://107.167.181.179:8080/api/v1/courses/keywords")
+json_data = json.loads(r.text)
+ParsedValue = json_data['data']
+for x in range(len(ParsedValue)):
+    print(ParsedValue[x])
+    url1 = "http://104.155.227.109:8080/api/v3/makeDirectory/"+str(ParsedValue[x])
+    r1 = requests.get(url1)
+    print(r1.text);
+    url2 = "http://104.155.227.109:8080/api/v3/makeKeywordContents/"+str(ParsedValue[x])
+    r2 = requests.get(url2)
+    print(r2.text);
+    url3 = "http://104.155.227.109:8080/api/v3/insertKeywordToSQL/"+str(ParsedValue[x])
+    r3 = requests.get(url3)
+    print(r3.text);
