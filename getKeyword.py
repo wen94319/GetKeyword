990aa82e874515e63fe3698e5b4016f985b00e54
歐博文 <wen94319@gmail.com>
2016-12-15 11:34:11 +0800

    加上每天要create Chaptermap

--- getKeyword.py
+++ getKeyword.py
@@ -1,5 +1,10 @@
 import requests
 import json
+import datetime
+
+now = datetime.datetime.now()
+print (datetime.datetime.now().strftime("%y"))
+
 r = requests.get("http://107.167.181.179:8080/api/v1/courses/keywords")
 json_data = json.loads(r.text)
 ParsedValue = json_data['data']
@@ -14,3 +19,6 @@ for x in range(len(ParsedValue)):
     url3 = "http://104.155.227.109:8080/api/v3/insertKeywordToSQL/"+str(ParsedValue[x])
     r3 = requests.get(url3)
     print(r3.text);
+    url4 = "http://104.155.227.109:8080/api/v1/createChapterMap/"+str(ParsedValue[x])
+    r4 = requests.get(url4)
+    print(r4.text);
